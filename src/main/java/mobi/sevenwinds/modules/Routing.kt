76755cc40df14afa659e5e86dafffd4efab10e38
Istomin Daniil <istomin.05.3@gmail.com>
2025-04-30 22:16:38 +0700

    Task 3 - update Swagger configuration

--- src/main/java/mobi/sevenwinds/modules/Routing.kt
+++ src/main/java/mobi/sevenwinds/modules/Routing.kt
@@ -7,9 +7,11 @@ import io.ktor.application.*
 import io.ktor.response.*
 import io.ktor.routing.*
 import mobi.sevenwinds.app.budget.budget
+import mobi.sevenwinds.app.author.author
 
 fun NormalOpenAPIRoute.swaggerRouting() {
     tag(SwaggerTag.Бюджет) { budget() }
+    tag(SwaggerTag.Авторы) { author() }
 }
 
 fun Routing.serviceRouting() {
